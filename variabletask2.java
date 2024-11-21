12208f090a0fb627715467632c63ed58bc07b35b
sanjayD17 <146454918+sanjayD17@users.noreply.github.com>
2024-11-21 12:54:39 +0530

    Add files via upload
    day 2

---
+++ variabletask2.java
@@ -0,0 +1,29 @@
+public class variabletask2 {
+public static void main(String arg[]) {
+String name1 = "Baki";
+String adjective1 = "brisk";
+String adjective2 = "wonderfull";
+String noun1 = "birds";
+String noun2 = "chocolate";
+String verb1 = "jump";
+String noun3 = "guitar";
+String noun4 = "children";
+String adjective3 = "excited";
+
+String name2 = "thahu";
+String place1 = "goa";
+
+String noun5 = "icecream";
+
+int number = 2050;
+
+String noun6 = "robots";
+
+
+
+
+
+String story = "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2 + " day!' Outside, a bunch of " + noun1+ "s were protesting to keep " + noun2 + " in stores. They began to " + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ". Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " + name1 + " in a puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", in a world where " + noun6 + "s ruled the world.";
+
+System.out.println(story);
+}}
