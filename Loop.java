098ec13cb2c3b1482f1181fbcf66aacc804b96c8
sanjayD17 <146454918+sanjayD17@users.noreply.github.com>
2024-11-26 13:15:55 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,37 @@
+package day3;
+import java.util.Scanner;
+public class Loop {
+	public static void main(String args[]) {
+//		Scanner lol = new Scanner(System.in);
+//		System.out.println("Enter the marks");
+//		int marks = lol.nextInt();
+//		if (marks>=35) {
+//			if (marks>=90) {
+//				System.out.println("Grade A");}
+//		    else if (marks>=70) {
+//		    	System.out.println("Grade B");
+//				}
+//		    else if (marks>=35) {
+//		    	System.out.println("Grade c");
+//				}
+//		    
+//		    }
+//		else {
+//	    	System.out.println("Fail");
+//		}}	 
+//		for(int god=0;god<=10;god++) {
+//		System.out.println(god);
+//		}
+		Scanner lol = new Scanner(System.in);
+		System.out.println("Enter the tables");
+		int tables=lol.nextInt();
+		for(int god=1;god<=10;god++) {
+		System.out.println(tables+"x"+god+"="+god*tables);
+}	
+		
+		
+	}
+	
+}
+
+
