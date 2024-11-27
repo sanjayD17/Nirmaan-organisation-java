630406b504bf6abd5f2faa3081d1a85d46dfd833
sanjayD17 <146454918+sanjayD17@users.noreply.github.com>
2024-11-27 13:04:34 +0530

    Add files via upload
    day6

---
+++ forloop.java
@@ -0,0 +1,22 @@
+package day6;
+
+public class forloop {
+public static void main(String args[]) {
+	for(int o=1;o<=100;o++) {
+		
+//		if (o%5==0) {
+//		System.out.println("LUCKY");
+//		}
+//		else {
+//			System.out.println(o);
+//		}
+
+		if (o%2==0) {
+			System.out.println("even : "+o);
+		}
+		else {
+			System.out.println("odd : "+o);
+		}
+	}
+}}
+
