fb8bfe53ad9b69102c7da4ab4b0ee1aa3785a21a
sanjayD17 <146454918+sanjayD17@users.noreply.github.com>
2024-12-03 14:35:40 +0530

    Add files via upload
    DAY7

---
+++ day7/conversion.java
@@ -0,0 +1,15 @@
+package day7;
+
+public class conversion {
+public static void main(String[] args) {
+     String name1 ="sanjay";
+     String name2 ="sanjay";
+	
+    if(name1.equals(name2)) {
+    	System.out.println("statement is true");
+    	
+    }else {
+    	System.out.println("statement is false");
+    }
+}
+}
---
+++ day7/forloop.java
@@ -0,0 +1,15 @@
+package day7;
+
+public class forloop {
+	public static void main(String[] args) {
+		for(int i=0;i<=3;i++) {
+			for(int j=0;j<=3;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	
+		
+	}
+
+}
