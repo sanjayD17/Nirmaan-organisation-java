602825f8c7484f52436994859cdde1c1808ab641
sanjayD17 <146454918+sanjayD17@users.noreply.github.com>
2024-12-06 23:36:02 +0530

    Update assesment1.java

--- day8/assesment1.java
+++ day8/assesment1.java
@@ -110,10 +110,49 @@ public static void main(String[] args) {
 	
 }
 //-----------------------------------------------------------------------------------
-
-	
+                             //find total and average
 //-----------------------------------------------------------------------------------
-
+//public static void main(String[] args) {
+// Scanner sc =new Scanner(System.in);
+// 
+// System.out.println("Enter the english mark : ");
+//int english=sc.nextInt();
+//System.out.println();
+//System.out.println("Enter the tamil mark : ");
+//int tamil=sc.nextInt();
+//System.out.println();
+//System.out.println("Enter the science mark : ");
+//int science=sc.nextInt();
+//System.out.println();
+//System.out.println("Enter the social mark : ");
+//int social=sc.nextInt();
+//System.out.println();
+//System.out.println("Enter the maths mark : ");
+//int maths=sc.nextInt();
+//System.out.println();
+//int total=(maths+english+tamil+social+science);
+// System.out.println("total : "+total );
+// float avg =total/5;
+// System.out.println("Average : "+avg );
+//}
+//-----------------------------------------------------------------------------------
+                                  //find a leap year
+//-----------------------------------------------------------------------------------
+	// void leap(int year) {
+	// 			if(year%4==0) {
+	// 				System.out.println("leap year");
+	// 			}
+	// 			else {
+	// 				System.out.println("not a leap year");
+	// 			}
+	// 		}
+	// 			public static void main(String[] args) {
+	// 				Scanner sc =new Scanner(System.in);
+	// 				day ans =new day();
+	// 				System.out.println("Enter the year : ");
+	// 				int year=sc.nextInt();
+	// 				ans.leap(year);
+				
 }
 }
 
