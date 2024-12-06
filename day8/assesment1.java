098c318182fca5e8b087c29cbea425bdca69c13e
sanjayD17 <146454918+sanjayD17@users.noreply.github.com>
2024-12-06 11:36:42 +0530

    assesment 1
    assesment 1

---
+++ day8/assesment1.java
@@ -0,0 +1,123 @@
+package day8;
+import java .util.Scanner;
+public class assesment1 {
+	
+             	//vote assesment
+//	           ------------------
+	//------------------------------------------------------------------------------------------------------
+//	void vote (int age){
+//		if(age>=18) {
+//			System.out.println("you are eligible to vote");
+//		}else {
+//			System.out.println("yore are not eligible to vote");
+//		}
+//	}
+	//public static void main(String[] args) {
+//		System.out.println("Enter the age : ");
+//Scanner sc = new Scanner(System.in);
+//	int age=sc.nextInt();
+//	assesment1 name =new assesment1();
+//	
+//	name.vote(age);
+	// ---------------------------------------------------------------------------------------------------
+
+	                   //	print 1 to 100 every mul shobld be "lucky"
+                   //   ---------------------------------------------------
+
+//------------------------------------------------------------------------------------------------------------
+ 
+//	public static void main(String[] args) {
+//		for (int i = 1;i<=100;i++) {
+//			if(i%4==0) {
+//				System.out.println("Lucky"); 
+//				
+//			}else {
+//				System.out.println(i);
+//			}
+//			
+//		}
+//	----------------------------------------------------------------------------------------------------------------
+		
+                                   //	skip odd numbers 1 to 70 
+//	                             ---------------------------------
+		
+//		----------------------------------------------------------------------------------------------------------------
+//		public static void main(String[] args) {
+//			for (int i = 1;i<=70;i++) {
+//				if(i%2==0) {
+//					System.out.println(i); 
+//					
+//				}else {
+//					continue;
+//				}
+//				
+//			}
+
+//--------------------------------------------------------------------------------------------------------------------------
+	                       //           check number is even or odd
+	                             //   --------------------------------
+	//-------------------------------------------------------------------------------------------------------------------------
+//	boolean istrue = true;
+//	public static void main(String[] args) {
+//		assesment1 dc =new assesment1();
+////		boolean istrue = true;
+//		Scanner sc = new Scanner(System.in);
+//		
+//		
+//		while(dc.istrue) {
+//			System.out.println("");
+//			System.out.println("Enter the number : ");
+//		int num = sc.nextInt();
+//		if (num%2==0) {
+//			System.out.println(num +" = is even  ");
+//			
+//		}else {
+//			System.out.println(num +" = is odd  ");
+//		}
+//		}
+// -------------------------------------------------------------------------------
+//                          string into captial to small
+// -------------------------------------------------------------------------------
+//	public static void main(String[] args) {
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("Enter the name UC: ");
+//		String name=sc.nextLine();
+//        System.out.println(name.toUpperCase());	
+//        System.out.println(" ");
+//        System.out.println("Enter the name LC: ");
+//        String name1=sc.nextLine();
+//        System.out.println(name1.toLowerCase());	
+ //    ------------------------------------------------------------------------------       
+	           // check mark is pass or fail
+//	-----------------------------------------------------------------------------------
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the mark : ");
+	int mark=sc.nextInt();
+	if(mark>=35) {
+		System.out.println("Pass");
+		if (mark>=90){
+		System.out.println("A grade");
+		
+		
+	}else if(mark>=70) {
+		System.out.println("B grade");
+	}
+	
+	
+	}else {
+		System.out.println("Fail");
+	
+}
+//-----------------------------------------------------------------------------------
+
+	
+//-----------------------------------------------------------------------------------
+
+}
+}
+
+
+
+
+
